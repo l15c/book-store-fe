710c34cdbb08a6c0c730eb66de0901dd9f2a401d
l15c <lehuuloc126@gmail.com>
2023-05-17 09:02:07 +0700

    Fix update avatar

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,13 +1,12 @@
 function genUrl(link: string, prefix?: string) {
-  return link.includes('http') ? link : `/api/cld/image${prefix && `/${prefix}`}/${link}`;
+  return link.includes('http') ? link : `/api/cld/image${prefix ? `/${prefix}` : ''}/${link}`;
 }
 
 export const getUrlImage = {
   product: (data: string, slug: string) => genUrl(data, `products/${slug}`),
   products: (data: string[], slug: string) => data.map((e) => genUrl(e, `products/${slug}`)),
 
-  avatar: (avatarUrl?: string) =>
-    avatarUrl ? genUrl(avatarUrl, 'users') : '/assets/avatar_default.jpg',
+  avatar: (avatarUrl?: string) => (avatarUrl ? genUrl(avatarUrl) : undefined), // '/assets/avatar_default.jpg'
 
   array: (data: string[], prefix?: string) => data.map((e) => genUrl(e, prefix)),
   single: (data: string, prefix?: string) => genUrl(data, prefix),
