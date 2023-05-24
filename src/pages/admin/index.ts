8512efccb62fc71f9310b543301d1c26641f55e8
l15c <lehuuloc126@gmail.com>
2023-05-24 14:57:04 +0700

    Fix #1

--- src/pages/admin/index.ts
+++ src/pages/admin/index.ts
@@ -1,29 +1,23 @@
 import { useEffect } from 'react';
 // next
 import { useRouter } from 'next/router';
-// config
-import { useAuthContext } from 'src/auth/useAuthContext';
-import { PATH_AFTER_LOGIN } from '../../config-global';
 // routes
 import { PATH_ADMIN } from '../../routes/paths';
 
 // ----------------------------------------------------------------------
 
 export default function Index() {
-  const { user } = useAuthContext();
   const { pathname, replace, prefetch } = useRouter();
 
   useEffect(() => {
     if (pathname === PATH_ADMIN.root) {
-      if (user?.roleId === 4) replace(PATH_ADMIN.invoice.root);
-      else replace(PATH_ADMIN.general.ecommerce);
+      replace(PATH_ADMIN.invoice.root);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
   useEffect(() => {
-    if (user?.roleId === 4) prefetch(PATH_ADMIN.invoice.root);
-    else prefetch(PATH_ADMIN.general.ecommerce);
+    prefetch(PATH_ADMIN.general.ecommerce);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
